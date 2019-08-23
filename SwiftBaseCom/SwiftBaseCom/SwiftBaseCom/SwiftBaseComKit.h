0e0cd68c385d5d3dcd50fbbfd5d7a10382a002ef
mac <1174724547@qq.com>
2019-08-23 17:38:04 +0800

    修改引用不到问题

---
+++ SwiftBaseCom/SwiftBaseCom/SwiftBaseCom/SwiftBaseComKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBaseComKit.h
+//  SwiftBaseComKit
+//
+//  Created by RWN on 2019/8/23.
+//  Copyright © 2019年 RWN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftBaseComKit.
+FOUNDATION_EXPORT double SwiftBaseComKitVersionNumber;
+
+//! Project version string for SwiftBaseComKit.
+FOUNDATION_EXPORT const unsigned char SwiftBaseComKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBaseComKit/PublicHeader.h>
+
+
